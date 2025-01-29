a98d40fb89a6413fbd8d2610bf2675d797dd3407
Pri04048 <priscila.s.silva12@aluno.senai.br>
2025-01-29 08:19:18 -0300

    Initial commit

---
+++ listas/listas.js
@@ -0,0 +1,27 @@
+let a = 4
+let b = 8
+
+
+let soma = a + b 
+console.log(soma)
+
+let sub = a - b 
+console.log(sub)
+
+let mult =  a * b 
+console.log(mult)
+
+
+let div = a / b 
+console.log(div)
+
+let resto = a % b 
+console.log(resto )
+
+
+
+let alt = 3
+let larg = 5
+
+let area = alt * larg 
+console.log(area)
