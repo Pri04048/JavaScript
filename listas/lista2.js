50b2029fe4abcfac31d49e606e2fba8f52a0c4bb
Pri04048 <priscila.s.silva12@aluno.senai.br>
2025-01-29 11:03:42 -0300

    alteração

---
+++ listas/lista2.js
@@ -0,0 +1,71 @@
+let num = 4
+let n = 2
+
+let resul = num > n
+
+let resul2 = num < n 
+
+let resul3 = num == n
+console.log(resul)
+console.log(resul2)
+console.log(resul3)
+
+
+let idade = 20
+let resultado = idade >= 16
+console.log(resultado)
+
+let resultado2 = idade >= 18
+console.log(resultado2)
+
+let resultado3 = idade <= 70
+console.log(resultado3)
+
+
+ let ida = 19
+
+ let result = ida >=18
+ let result2 = ida >= 60
+
+ console.log(result)
+ console.log(result2)
+
+
+ let prog = "usuario"
+
+ let nome =prog== "admin" 
+
+ console.log(nome)
+
+ let numero = 4
+
+let  d =  numero % 3 == 0
+
+let d2 =  numero  % 5 == 0 
+
+console.log(d)
+
+console.log(d2)
+
+let saldo = 200
+let saque = 300
+
+let total =  saldo - saque
+let situacao = total > 0
+console.log(situacao)
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
--- listas/listas.js
+++ listas/listas.js
@@ -25,3 +25,20 @@ let larg = 5
 
 let area = alt * larg 
 console.log(area)
+
+let num = 4
+let dobr = num*2
+console.log(dobr)
+let met = num / 2
+console.log(met)
+
+
+let not = 2
+let not2 = 5
+let not3 = 3
+ let con = (2 + 5 + 3)/3
+ console.log(con)
+ 
+ let cel = 10
+  let fah = 10* 9/5 + 32
+  console.log(fah)
