5bd0c5f0d79c0a1db3811cc773833e78a82a5743
Pri04048 <priscila.s.silva12@aluno.senai.br>
2025-01-29 09:27:44 -0300

    atualização

---
+++ Operadores/relacionais.js
@@ -0,0 +1,40 @@
+/* > maior
+>= maior ou igual 
+< menor
+<= menor ou igual 
+== igual 
+!= diferente */
+
+let idade = 18
+let resultado = idade > 18//MAIOR
+let resultado2 = idade >= 18//MAIOR OU IGUAL
+let resultado3 = idade < 18//MENOR
+let resultado4 = idade <= 18//MENOR OU IGUAL
+let resultado5 = idade == 18//IGAUL
+let resultado6 = idade != //DIFERENTE
+console.log(resultado)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
+console.log(resultado6)
+
+
+
+console.log("---------------")
+console.log("Outro exemplo")
+let dinheiro = 100
+let compra = 40
+let total =  dinheiro - compra
+let situacao = total > 0
+console.log(situacao)
+console.log("---------------")
+console.log("Outro exemplo")
+//Saber se um numero  é divisivel por outro 
+let numero = 10
+let divisivel = 10 % 2 
+console.log(divisivel)
+
+
+
+
