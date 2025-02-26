545ad728d20e63fa8fc7940a2f96f1f32be3051b
Pri04048 <priscila.s.silva12@aluno.senai.br>
2025-02-26 11:18:01 -0300

    alteração

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,9 +1,28 @@
 //ARQUIVO DOM.JS
-let imagem = document.getElementById("imagem")
+// let imagem = document.getElementById("imagem")
 
-imagem.addEventListener("mouseover", function () {
-    imagem.setAttribute("src" , "flor2.webp")
+// imagem.addEventListener("mouseover", function () {
+//     imagem.setAttribute("src" , "flor2.webp")
+// })
+// imagem.addEventListener("mouseout" , function(){
+//     imagem.setAttribute("src" , "flor1.jpg")
+// })
+
+//-------------------------------------------------------
+let lista = document.getElementById("lista")
+let adicionar = document.getElementById("adicionar")
+let remover = document.getElementById("remover")
+
+adicionar.addEventListener("click" , function() {
+    let novoItem = document.createElement("li")
+    novoItem.innerText = "Novo item"
+    lista.appendChild(novoItem)
 })
-imagem.addEventListener("mouseout" , function(){
-    imagem.setAttribute("src" , "flor1.jpg")
+remover.addEventListener("click" , function() {
+    if (lista.lastChild) {
+        lista.removeChild(lista.lastChild)
+    }
 })
+
+//-----------------------------------------------------------
+
---
+++ listas/lista14.js
@@ -0,0 +1,70 @@
+//ex1
+let botton = document.getElementById("botton")
+
+botton.addEventListener("click", function(){
+   botton.style.backgroundColor = "pink"
+})  
+botton.addEventListener("dblclick" , function (){
+botton.style.backgroundColor = "purple"
+})
+
+//ex2
+let like = document.getElementById("like")
+let para = document.getElementById("para")
+let curtir = 0
+like.addEventListener("click" , function() {
+ 
+   curtir++
+   para.innerText=curtir
+})
+
+//ex3
+let par1 = document.getElementById("par1")
+let par2 = document.getElementById("par2")
+let par3 = document.getElementById("par3")
+let par4 = document.getElementById("par4")
+let par5 = document.getElementById("par5")
+let bot = document.getElementById("bot")
+bot.addEventListener("click" , function() {
+    par1.innerText="Eu gosto de JavaScript"
+     par2.innerText="Eu gosto de JavaScript"
+      par3.innerText="Eu gosto de JavaScript"
+       par4.innerText="Eu gosto de JavaScript"
+        par5.innerText="Eu gosto de JavaScript"
+
+})
+
+//ex4
+let lais = document.getElementById("lais")
+let botao = document.getElementById("botao")
+botao.addEventListener("click", function(){
+if (lais.lastChild) {
+    lais.removeChild(lais.lastChild)
+    botao.innerText = "Mensagem Enviada"
+} 
+})
+
+//ex5
+let tex1 = document.getElementById("tex1")
+let aju = document.getElementById("aju")
+aju.addEventListener("click" ,   function() {
+    tex1.style.display = "flex"
+})
+
+//ex6
+let div = document.getElementById("div")
+let bot2 = document.getElementById("bot2")
+let bot3 = document.getElementById("bot3")
+bot2.addEventListener("click" , function (){
+if(div.style.width = "200px")
+    (div.style.height = "200px")
+(div.style.backgroundColor = "pink")
+})
+bot3.addEventListener("click" , function (){
+    if(div.style.borderRadius ="100px" )
+        (div.style.backgroundColor = "purple")
+})
+
+magem.style.width = "200px"
+    imagem.style.heigth = "200px"
+
