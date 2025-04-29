d61e20439ef7c5e24884403ad1689b252d9fb8c4
Pri04048 <priscila.s.silva12@aluno.senai.br>
2025-04-29 11:32:08 -0300

    alteração

---
+++ loja/script.js
@@ -0,0 +1,115 @@
+const listaProdutos = [
+    {id:1, nome:"Blush", preco: 199, imagem:"img/Produto1.jpg"},
+    {id:2, nome:"Halo Glow", preco: 145, imagem:"img/Produto2.jpg"},
+    {id:3, nome:"Gloss Shine", preco: 89, imagem:"img/Produto3.jpg"},
+    {id:1, nome:"Pink Iluminador", preco: 120, imagem:"img/Produto4.jpg"}
+]
+
+//Pegando elementos do HTML
+const containerProdutos = document.getElementById("produtos")
+const barraPesquisa = document.getElementById("barraPesquisa")
+const listaCarrinho = document.getElementById("listaCarrinho")
+const botaoCarrinho = document.getElementById("botaoCarrinho")
+const botaoLoja = document.getElementById("botaoLoja")
+const divCarrinho = document.getElementById("carrinho")
+const totalCarrinho = document.getElementById("totalCarrinho")
+
+//Carrinho de compras 
+let carrinho = []
+//Função que mostra  os produtos
+function mostrarProdutos(lista){
+    containerProdutos.innerHTML = ""
+    lista.forEach(produto => {
+        const card = document.createElement("div")
+        card.className = "card"
+        card.innerHTML = `
+        <img src="${produto.imagem}" alt="${produto.nome}">
+        <h3>${produto.nome}</h3>
+        <p>${produto.preco}</p>
+        <button onclick="adicionarCarrinho(${produto.id})">Adicionar ao Carrinho</button>
+       `
+       containerProdutos.appendChild(card)
+    })      
+}
+//Adicionar ao carrinho 
+function adicionarCarrinho(idProduto){
+    const produtoSelecionado = listaProdutos.find(prod =>  prod.id === idProduto)
+    const itemExistente = carrinho.find(item => item.id === idProduto)
+     if (itemExistente){
+        itemExistente.quantidade +=1
+    }else{
+        carrinho.push({...produtoSelecionado, quantidade: 1})
+    }
+    atualizarCarrinho()
+
+}
+//Diminuir a quantidade no carrinho 
+function diminuirQuantidade(idProduto){
+    const itemExistente = carrinho.find(item => item.id === idProduto)
+    if(itemExistente) {
+        itemExistente.quantidade -=1
+        if(itemExistente.quantidade <=0) {
+            removerDoCarrinho(idProduto)
+            return
+        }
+        atualizarCarrinho()
+    }
+}
+//Remover do xcarrinho
+function removerDoCarrinho(idProduto){
+    carrinho = carrinho.filter(item => item.id!== idProduto)
+    atualizarCarrinho()
+}
+//Atualizar carrinho
+function atualizarCarrinho(){
+    listaCarrinho.innerText =""
+    carrinho.forEach(item => {
+        const li = document.createElement("li")
+        li.innerHTML = `
+        <span>${item.nome}</span>
+        <span>${item.quantidade}</span>
+        <span>${item.preco * item.quantidade},00</span>
+        <span>
+        <button class="aumentar" onclick="adicionarCarrinho(${item.id})">+</button>
+        <button class="diminuir" onclick="diminuirQuantidade(${item.id})">-</button>
+         <button class="remover" onclick="removerDoCarrinho(${item.id})">Remover</button>
+         </span>
+        `
+        listaCarrinho.appendChild(li)
+    })
+}
+//calcular total a pagar 
+function calcularTotal(){
+    let total = 0
+    carrinho.forEach(item => {
+        total += item.preco * item.quantidade
+    })
+    totalCarrinho.innerHTML = `<strong>Total: R$ ${total}, 00</strong>`
+}
+// Barra de pesqusa
+barraPesquisa.addEventListener("input", () => {
+    const textoPesquisa = barraPesquisa.value.toLowerCase()
+    const produtosFiltrados = listaProdutos.filter(produto =>
+        produto.nome.toLocaleLowerCase().includes(textoPesquisa)
+    )
+    mostrarProdutos(produtosFiltrados)
+})
+//Botão ver carrinho 
+botaoCarrinho.addEventListener("click", () => {
+    containerProdutos.style.display = "none"
+    barraPesquisa.style.display = "none"
+    divCarrinho.style.display = "block"
+    botaoCarrinho.style.display = "none"
+    botaoLoja.style.display = "inline"
+})
+
+//Botao voltar para loja 
+botaoLoja.addEventListener("click", () => {
+    containerProdutos.style.display = "flex"
+    barraPesquisa.style.display = "block"
+    divCarrinho.style.display = "none"
+    botaoCarrinho.style.display = "inline"
+    botaoLoja.style.display = "none"
+})
+//inicialização 
+mostrarProdutos(listaProdutos)
